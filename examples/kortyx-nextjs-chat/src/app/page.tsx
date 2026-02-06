408f17f95a659e52ac216285f3ad9dc4b59132ee
Mustafa ONAL <mustafasaitonal@outlook.com>
2026-02-06 15:19:12 +0100

    fix: remove lint error

--- examples/kortyx-nextjs-chat/src/app/page.tsx
+++ examples/kortyx-nextjs-chat/src/app/page.tsx
@@ -4,7 +4,6 @@ import { ChatWindow } from "@/components/features/chat/chat-window";
 import { ChatProvider } from "@/context/chat-context";
 
 export default function Home() {
-  const test: any = "";
   return (
     <ChatProvider>
       <div className="h-screen w-screen flex items-center justify-center">
